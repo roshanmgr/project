7b3be57b6b6062e5d89b4c94ee0dcd721f950a69
unknown <roshanmgr130@gmail.com>
2022-03-10 20:46:41 +0545

    front end

---
+++ a-footer.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="./css/a-footer.css">
+
+</head>
+<body>
+    
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#" class="links"> <i class="fas fa-phone"></i> Founder: Roshan Thapa Magar</a>
+             <a href="#" class="links"> <i class="fas fa-phone"></i> Co-founder: Sumjyo Hyolmo </a>
+            <a href="#" class="links"> <i class="fas fa-phone"></i> 9818712174 </a>
+            <a href="#" class="links"> <i class="fas fa-envelope"></i> roshanmgr130@gmail.com </a>
+            <a href="#" class="links"> <i class="fas fa-map-marker-alt"></i> kathmandu nepal </a>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#home" class="links"> <i class="fas fa-arrow-right"></i> Home </a>
+            <a href="#Aboutus" class="links"> <i class="fas fa-arrow-right"></i> About Us </a>
+            <a href="#service" class="links"> <i class="fas fa-arrow-right"></i> Service </a>
+            <a href="#contactus" class="links"> <i class="fas fa-arrow-right"></i> Contact Us </a>
+         </div>
+
+    </div>
+
+    <div class="credit"> Copyright &copy; 2022|2079 www.sahayogi.com. All rights reserved!</div>
+
+</section>
+
+<!-- footer section ends -->
+</body>
+</html>
